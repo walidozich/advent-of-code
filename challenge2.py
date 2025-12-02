3f9245c2d9d49b874d11f31ddeb30e2fa950c6e6
cherchali mohamed walid <cherchalimohamedwalid@gmail.com>
2025-12-02 15:09:53 +0100

    part 2 ch2

--- challenge2.py
+++ challenge2.py
@@ -55,5 +55,53 @@ def main(argv):
 
 
 if __name__ == '__main__':
-	main(sys.argv)
+	# allow `part2` argument to compute the new rules (sequence repeated >=2 times)
+	if len(sys.argv) > 1 and sys.argv[1] in ('part2', '--part2'):
+		# build list of ranges
+		fname = 'sample.txt'
+		data = open(fname, 'r', encoding='utf-8').read().strip()
+		ranges = list(parse_ranges(data))
+
+		max_b = max(b for _, b in ranges)
+
+		# generate all numbers that are a repetition of some sequence (r>=2)
+		nums = set()
+		max_len = len(str(max_b))
+		for k in range(1, max_len + 1):
+			# r must be at least 2 and k*r <= max_len
+			rmax = max_len // k
+			if rmax < 2:
+				continue
+			t_min = 10 ** (k - 1)
+			t_max = 10**k - 1
+			for r in range(2, rmax + 1):
+				# multiplier = (10^{k*r}-1)/(10^k-1)
+				mult = (10 ** (k * r) - 1) // (10 ** k - 1)
+				# t * mult <= max_b => t <= max_b // mult
+				high_t = min(t_max, max_b // mult)
+				if high_t < t_min:
+					continue
+				for t in range(t_min, high_t + 1):
+					n = t * mult
+					# ensure no leading zeros (t_min takes care) and n <= max_b
+					if n <= max_b:
+						nums.add(n)
+
+		nums_list = sorted(nums)
+		# prefix sums
+		from bisect import bisect_left, bisect_right
+
+		pref = [0]
+		for v in nums_list:
+			pref.append(pref[-1] + v)
+
+		total2 = 0
+		for a, b in ranges:
+			i = bisect_left(nums_list, a)
+			j = bisect_right(nums_list, b)
+			total2 += pref[j] - pref[i]
+
+		print(total2)
+	else:
+		main(sys.argv)
 
